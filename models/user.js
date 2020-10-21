916789fc18c4bf431485aafd37b1e9ac4d208bbd
Brad Hoeren <brad@unival.com>
2020-10-20 19:09:25 -0700

    First commit!

---
+++ models/user.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    }
+  }, {});
+  return User;
+};
